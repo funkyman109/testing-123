5657c2ae3121772a09911b5965504c8b15a1ea79
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-04-13 19:25:25 -0400

    Delete my_script.py

--- my_script.py
+++
@@ -1,8 +0,0 @@
-def enlarge(i):
-    return i * 100
-
-if __name__ == "__main__":
-  ""
-  my_number = float(input("please input a number: "))
-  n = enlarge(my_number)
-  print("enlarged number=", n)
