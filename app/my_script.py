7bce4ccd2ce7688361c75ddff9af213168f68814
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-04-13 19:25:32 -0400

    Create my_script.py

---
+++ app/my_script.py
@@ -0,0 +1,8 @@
+def enlarge(i):
+    return i * 100
+
+if __name__ == "__main__":
+  ""
+  my_number = float(input("please input a number: "))
+  n = enlarge(my_number)
+  print("enlarged number=", n)
